c5a369b9b2822f81bd6d6fbc07f46dadddf182f4
St-x <St-x@neuve.ch>
2018-05-16 19:57:23 +0200

    Creation class Test

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by Ernest Rw on 16.05.18.
+//  Copyright © 2018 Ernest Rw. All rights reserved.
+//
+
+import Foundation
+
+
+class Test {
+    func salut() {
+        print("Salut la compagnie")
+    }
+}
